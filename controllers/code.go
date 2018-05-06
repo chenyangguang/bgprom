0030e279cb473552677a72baf3d5d05946b461f2
yangguang_chen <yangguang_chen@kingdee.com>
2018-05-06 17:12:39 +0800

    * prometheus type summary

--- controllers/code.go
+++ controllers/code.go
@@ -1,8 +1,11 @@
 package controllers
 
 import (
+	"math"
+
 	"github.com/astaxie/beego"
 	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
 )
 
 var (
@@ -12,6 +15,12 @@ var (
 			Help: "total request code controller",
 		},
 	)
+
+	temps = prometheus.NewSummary(prometheus.SummaryOpts{
+		Name:       "pond_temperature_celsius",
+		Help:       "The temperature of the frog pond.",
+		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+	})
 )
 
 type CodeController struct {
@@ -20,10 +29,19 @@ type CodeController struct {
 
 func init() {
 	prometheus.MustRegister(codeCounter)
+	prometheus.MustRegister(temps)
+
 }
 
 func (c *CodeController) Get() {
 	codeCounter.Inc()
+
+	// Simulate some observations.
+	for i := 0; i < 1000; i++ {
+		temps.Observe(30 + math.Floor(120*math.Sin(float64(i)*0.1))/10)
+	}
+	metric := &dto.Metric{}
+	temps.Write(metric)
 	c.TplName = "code.tpl"
 
 }
